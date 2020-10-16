50378a7386bdb84d5258a372c6f40df1bcdc7691
Catherine Gloria <catherinegloria@gmail.com>
2020-10-16 22:41:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   def add_item(item, amount, quantity = 1)
     self.total += amount * quantity
     @items.fill(item, @items.size, quantity)
-    @last_transaction = amount * quantity
+    @last_transaction = (amount * quantity).to_f
   end
 
   def apply_discount
