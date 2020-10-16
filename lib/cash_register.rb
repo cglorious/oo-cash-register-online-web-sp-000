6a5d21541d251585fdd3c9b33b64445b5247574f
Catherine Gloria <catherinegloria@gmail.com>
2020-10-16 22:08:04 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
+class CashRegister
 
+  attr_accessor :discount, :total, :items
+
+  def initialize(discount = 0)
+    @discount = discount
+    @total = 0
+    @items = []
+  end
+
+  def add_item(item, amount, quantity = 1)
+    self.total += amount * quantity
+    @items << item #account for quantity
+  end
+
+  def apply_discount
+    self.total = (@total * ((100 - @discount.to_f)/100)).to_i
+    if @discount > 0
+      "After the discount, the total comes to $#{self.total}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def void_last_transaction
+    #@items[@items.length - 1] last item
+    #delete amount from total
+    #self.total - 
+  end
+
+end
