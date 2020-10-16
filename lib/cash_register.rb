51569d812abf479335e2134686ee79b589ab3612
Catherine Gloria <catherinegloria@gmail.com>
2020-10-16 22:19:26 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -25,7 +25,7 @@ class CashRegister
   end
 
   def void_last_transaction
-    #@items[@items.length - 1] - last item
+    #@amounts[@amounts.length - 1] - last amount
     #delete amount from total
     #self.total - amount of last item
   end
